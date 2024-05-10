50b759a9d7937f420667d4b4e06225f461e6c920
Priyanshu Negi <npriyanshu63@gmail.com>
2024-05-10 13:24:12 +0530

    added auth secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
 
+authOptions.secret = process.env.SECRET;
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
